85dfd089d6836aaea915fc40ee20f07505960fb0
Maxime <maxime.yoshi@hotmail.fr>
2025-02-07 16:00:42 +0100

    First commit

---
+++ src/test/java/com/api/safetynet/SafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
